3231e70646ff857a44497145ecd69822ec178ae3
Filipe Costa <filipe@rhizatech.com.br>
2024-12-24 11:05:29 -0300

    FEATURE // Last static files

---
+++ static/main.js
@@ -0,0 +1,53 @@
+addEventListener("load", (event) => {});
+
+onload = (event) => {
+  // slider js
+  silder();
+
+  // sticky hedder
+  stickystickyHeader();
+
+// search controller
+  search();
+
+// mobile menu
+  drawer();
+
+// accordion
+  accordions();
+
+// quick view modal
+  modal();
+
+//glightbox
+  GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: false,
+    selector: ".glightbox",
+    slideEffect: "fade",
+    videosWidth: "800px",
+  });
+  GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: false,
+    selector: ".glightbox2",
+    slideEffect: "fade",
+    videosWidth: "800px",
+  });
+  GLightbox({
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: false,
+    selector: ".glightbox3",
+    slideEffect: "fade",
+    videosWidth: "800px",
+  });
+
+// scrollUp
+  scrollUp();
+
+// smooth scroll
+  smoothScroll();
+};
