21658f88cb568ff794c01088e7773792371e0fd3
Filipe Costa <filipe@rhizatech.com.br>
2024-12-26 13:33:25 -0300

    FEATURE // Scroll event at the ScroolTop component, now it is hidden automatically depending on how close the top the window is

--- components/VueScrollTop.vue
+++ components/VueScrollTop.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- scroll top -->
-  <button @click.prevent="scrollUp()"
+  <button @click.prevent="scrollUp()" ref="button"
     class="scroll-up w-30px h-30px lg:w-10 lg:h-10 lg:text-xl bg-section-bg-1 text-heading-color hover:bg-secondary-color hover:text-white rotate-[45deg] shadow-box-shadow-3 fixed bottom-[50px] lg:bottom-[70px] right-[3%] flex justify-center items-center z-xl active">
     <font-awesome-icon icon="fa fa-angle-up" class="leading-1 -rotate-[45deg] inline-block"/>
   </button>
@@ -8,6 +8,18 @@
 <script>
 export default {
   name: 'VueScrollTop',
+  mounted() {
+    window.addEventListener("scroll", () => {
+      const scrollCount = window.scrollY;
+
+      if (scrollCount < 300) {
+        this.$refs.button.classList.remove("active");
+      }
+      if (scrollCount > 300) {
+        this.$refs.button.classList.add("active");
+      }
+    });
+  },
   methods: {
     scrollUp: () => {
         window.scroll({ top: 0, left: 0, behavior: "smooth" });
